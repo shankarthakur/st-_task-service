f2a6d1fb6b5b20efaf33fb64e52c43360dc44ce1
shankar thakur <shankarthaur07@gmail.com>
2024-06-30 12:17:03 +0530

    first commit

---
+++ src/main/java/com/supreme/TaskSeviceApplication.java
@@ -0,0 +1,13 @@
+package com.supreme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSeviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSeviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supreme/TaskSeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supreme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
